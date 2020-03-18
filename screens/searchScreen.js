3a25696be2ac493652c048e6342e711069b60b74
Gregory Plazas <gplaz005@fiu.edu>
2020-03-17 20:35:29 -0400

    search display cat id

--- screens/searchScreen.js
+++ screens/searchScreen.js
@@ -1,4 +1,6 @@
 /*import { SearchBar } from 'react-native-elements';
+import React from 'react';
+import { View, Text } from 'react-native';
 
 export default class App extends React.Component {
   state = {
@@ -13,11 +15,13 @@ export default class App extends React.Component {
     const { search } = this.state;
 
     return (
+      <View>
       <SearchBar
         placeholder="Type Here..."
         onChangeText={this.updateSearch}
         value={search}
       />
+    <Text>{search}</Text></View>
     );
   }
 }
@@ -28,41 +32,58 @@ export default class App extends React.Component {
 
 
 
+
 import React, { useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button , ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button , FlatList } from 'react-native';
+import {CONTENT, SUBCATEGORIES} from '../data/categoriesData';
 
 export default function App() {// to be call Search
 
   const [enteredSearch, setEnteredSearch] = useState('');
-  const [searches, setSeacrhes] = useState([]);
-
+  const [search, setSearch] = useState([]);
+  const [data, setData] = useState([
+    'Mark','Luke','peter'
+  ]);
+  const [result, setResult] = useState('p');//just use any value cuz if empty .filter will display all
+ 
   
-  const searchInputHandler = (enteredText) =>{
-
+  
+  let res = '';
+  const searchInputHandler = (enteredText) =>{  
     setEnteredSearch(enteredText)
   };
 
   const addToSearches = () => {
-
-    setSeacrhes(currentSearch => [...searches, enteredSearch] );
-  
+    setSearch(currentSearch => [...search, enteredSearch] );
   };
+
+  const displaySub = SUBCATEGORIES.filter(cat => cat.subId.indexOf(result) >= 0);
+
+  const mody = () => {
+    setResult(search)
+   
+    };
+    
+  
 
   return (
     <View  style = {styles.screen}>
       <View style = {styles.inputContainer}>
         <TextInput placeholder = "search..." 
         style = {styles.input} onChangeText = {searchInputHandler} Value = {enteredSearch}/>
-        <Button title = "Search" style = {styles.searchButton} onPress = {addToSearches} />  
+        <Button title = "Add" style = {styles.searchButton} onPress = {addToSearches} /> 
+        <Button title = "search" style = {styles.searchButton} onPress = {mody} />
+         
       </View>
-        
-        <ScrollView>
-        {searches.map((goal) =>(
-          <View key = {goal}>
-            <Text>{'Results for: '}{goal}</Text>
-          </View>
-        ))}
-        </ScrollView>
+      <View><Text>{search}</Text></View>
+      <View><Text>{result}</Text></View>
+
+      <View>
+      <FlatList data = {displaySub} renderItem ={({item}) => 
+      <Text>{item.title}</Text>} numColumns={1}/>
+      </View>
+      
+   
       
     </View>
   );
