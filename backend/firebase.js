742b43a8021d8ef5fc1d99354daa700902723974
Andre Marin <amari087@fiu.edu>
2020-04-21 14:33:25 -0400

    Deleted API key and other info

--- backend/firebase.js
+++ backend/firebase.js
@@ -16,11 +16,11 @@ class Firebase {
   init = () => {
     if (!firebase.apps.length) {
       const firebaseConfig = {
-        apiKey: "AIzaSyC7THmTy5jrNwAW_BBk1xtQBLCqKzmY9TM",
-        authDomain: "pemapp-9eba9.firebaseapp.com",
-        databaseURL: "https://pemapp-9eba9.firebaseio.com",
-        projectId: "pemapp-9eba9",
-        storageBucket: "pemapp-9eba9.appspot.com",
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
       };
 
       firebase.initializeApp(firebaseConfig);
